c1b81c8c376a7eea4962befa56454c31bfec7df9
Alok Suman <71497735+Alokray007@users.noreply.github.com>
2023-09-25 23:59:41 +0530

    1st commit

---
+++ problems/17-arrow-reverse.js
@@ -0,0 +1,27 @@
+/***********************************************************************
+Write a function reverseStr(str) that accepts a string and returns that string
+reversed.
+
+Write this function using a fat arrow function!
+
+Examples:
+let result1 = reverseStr("hello"); // returns "olleh"
+let result2 = reverseStr("garden"); // returns "nedrag"
+let result3 = reverseStr("potato"); // returns "otatop"
+
+
+***********************************************************************/
+let reverseStr = (str) => {
+  let newstr = str.split("");
+  let result = newstr.reverse();
+  let reversed = result.join("");
+  return reversed;
+}
+
+
+/**************DO NOT MODIFY ANYTHING UNDER THIS LINE*****************/
+try {
+    module.exports = reverseStr;
+  } catch (e) {
+    return null;
+  }
